afb1adc7a36cfab9002ded48900ada0adfb1d106
Griffin <omondigriffin4@gmail.com>
2024-03-03 00:19:43 +0300

    creates a script that runs different commands

--- Python_programming/user_input/codechallenge2.py
+++ Python_programming/user_input/codechallenge2.py
@@ -1,34 +1,34 @@
-# integers = input("Enter integers")
-# integerLists = list(integers)
+integers = input("Enter integers")
+integerLists = list(integers)
  
  
-# # using loop
-# for i in range(0, len(integerLists)):
-#     integerLists[i] = int(integerLists[i])
+# using loop
+for i in range(0, len(integerLists)):
+    integerLists[i] = int(integerLists[i])
  
-# # Printing modified list
-# print("Sum of list entered is : " + str(sum(integerLists)))
+# Printing modified list
+print("Sum of list entered is : " + str(sum(integerLists)))
 
 
-# favorite_books = ("Born a Crime", "The Lean startup", "The making of a manager", "Steps to Christ")
-# for book in favorite_books:
-#     print (book)
+favorite_books = ("Born a Crime", "The Lean startup", "The making of a manager", "Steps to Christ")
+for book in favorite_books:
+    print (book)
 
-# person_details = {}
-# person_details["name"] = input("What is your name: ")
-# person_details["age"] = input("How old are you: ")
-# person_details["favorite_color"] = input("What is your favorite color: ")
+person_details = {}
+person_details["name"] = input("What is your name: ")
+person_details["age"] = input("How old are you: ")
+person_details["favorite_color"] = input("What is your favorite color: ")
 
-# print (person_details)
+print (person_details)
 
-# integer1 = input("Enter integers")
-# integer_set = set(integer1)
-# print(integer_set)
-# integer2 = input("Enter integers")
-# integer_set2 = set(integer2)
-# print (integer_set2)
+integer1 = input("Enter integers")
+integer_set = set(integer1)
+print(integer_set)
+integer2 = input("Enter integers")
+integer_set2 = set(integer2)
+print (integer_set2)
 
-# print(integer_set2.intersection(integer_set))
+print("The common numbers in the two sets are", integer_set2.intersection(integer_set))
 
 words = ["come", "go", "peter", "bring", "Monday", "water"]
 odd_number_characters = [word for word in words if len(word) % 2 != 0 ]
