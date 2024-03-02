1ed4ce2a3bb318ba8f7a4644b6dcea6be8432d07
Griffin <omondigriffin4@gmail.com>
2024-03-02 23:42:31 +0300

    creates a program that prints out a list of books

---
+++ Python_programming/user_input/codechallenge2.py
@@ -0,0 +1,16 @@
+# integers = input("Enter integers separated by spaces")
+# integerLists = list(integers)
+ 
+ 
+# # using loop
+# for i in range(0, len(integerLists)):
+#     integerLists[i] = int(integerLists[i])
+ 
+# # Printing modified list
+# print("Sum of list entered is : " + str(sum(integerLists)))
+
+
+# favorite_books = ("Born a Crime", "The Lean startup", "The making of a manager", "Steps to Christ")
+# for book in favorite_books:
+#     print (book)
+
--- Python_programming/user_input/user_input.py
+++ Python_programming/user_input/user_input.py
@@ -3,3 +3,7 @@ age = input ("How old are you? ")
 location = input ("Where do you stay? ")
 
 print(f"Hello {name} you are {age} years old and live in {location}")
+
+
+
+
