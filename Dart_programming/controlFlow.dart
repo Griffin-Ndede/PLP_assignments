b7b5aba9340f48b962fc83bd68d9796e63aa283b
Griffin <griffin.omondi@student.moringaschool.com>
2024-04-11 15:31:24 +0300

    creates a dart control flow function

---
+++ Dart_programming/controlFlow.dart
@@ -0,0 +1,15 @@
+void checkNumber(int num) {
+  if (num > 10) {
+    print("Your number is greater than 10");
+  } else if (num == 10) {
+    print("Your number is equal to 10");
+  } else {
+    print("Your number is less than 10");
+  }
+}
+
+void main() {
+  checkNumber(9);
+  checkNumber(20);
+  checkNumber(10);
+}
