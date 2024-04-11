b0ad67484182424b973ec805409206287cbb672f
Griffin <griffin.omondi@student.moringaschool.com>
2024-04-11 12:27:01 +0300

    solves all the assignment questions

--- Dart_programming/functionsAssignment.dart
+++ Dart_programming/functionsAssignment.dart
@@ -13,8 +13,25 @@ void multiplyTwo(int num1, int num2) {
   print(result);
 }
 
+void divideTwo(int num1, int num2) {
+  double result = num1 / num2;
+  print(result);
+}
+
+void stringLength(String inputString) {
+  int length = inputString.length;
+  print(length);
+}
+
+dynamic getFirtsElement(List<dynamic> list) {
+  print(list.first);
+}
+
 void main() {
   addTwo(10, 5);
   subtractTwo(5, 9);
   multiplyTwo(20, 30);
+  divideTwo(8, 4);
+  stringLength("Hello world");
+  getFirtsElement(["mango", "Griffin"]);
 }
