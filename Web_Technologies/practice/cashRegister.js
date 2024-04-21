b8a727b588bfd0020b06c1648cad4624214a4b31
Griffin <omondigriffin4@gmail.com>
2024-04-21 15:33:26 +0300

    creates an event listener and innert hmtl

---
+++ Web_Technologies/practice/cashRegister.js
@@ -0,0 +1,46 @@
+const cashRegister = {
+    itemsForSale: {
+      Phone: 300,
+      "Smart TV": 220,
+      "Gaming Console": 150,
+    },
+    shoppingCart: [],
+    addItem: function (itemName) {
+      if (this.itemsForSale[itemName] !== undefined) {
+        this.shoppingCart.push(itemName);
+        console.log(`${itemName} added to the shopping cart.`);
+      } else {
+        console.log(`We don't sell ${itemName}.`);
+      }
+    },
+    calculateTotalPrice: function () {
+      let totalPrice = 0;
+      for (let item of this.shoppingCart) {
+        totalPrice += this.itemsForSale[item];
+      }
+      return totalPrice;
+    },
+    pay: function (paymentAmount) {
+      let totalPrice = this.calculateTotalPrice();
+      let discount = 0;
+      if (totalPrice > 400) {
+        discount = totalPrice * 0.1;
+      }
+      totalPrice -= discount;
+      console.log(`Total price: $${totalPrice}`);
+      if (paymentAmount >= totalPrice) {
+        let change = paymentAmount - totalPrice;
+        console.log(`Thank you for your purchase! Your change is $${change}.`);
+      } else {
+        console.log(`You don't have enough money to purchase the items.`);
+      }
+    },
+  };
+  
+  // Test the cash register
+  cashRegister.addItem("Iphone");
+  cashRegister.addItem("Smart TV");
+  cashRegister.addItem("Phone")
+  cashRegister.addItem("Gaming Console");
+  console.log(`Total price: $${cashRegister.calculateTotalPrice()}`);
+  cashRegister.pay(800);
--- Web_Technologies/practice/practice.js
+++ Web_Technologies/practice/practice.js
@@ -1,47 +1,25 @@
-const cashRegister = {
-    itemsForSale: {
-      Phone: 300,
-      "Smart TV": 220,
-      "Gaming Console": 150,
-    },
-    shoppingCart: [],
-    addItem: function (itemName) {
-      if (this.itemsForSale[itemName] !== undefined) {
-        this.shoppingCart.push(itemName);
-        console.log(`${itemName} added to the shopping cart.`);
-      } else {
-        console.log(`We don't sell ${itemName}.`);
-      }
-    },
-    calculateTotalPrice: function () {
-      let totalPrice = 0;
-      for (let item of this.shoppingCart) {
-        totalPrice += this.itemsForSale[item];
-      }
-      return totalPrice;
-    },
-    pay: function (paymentAmount) {
-      let totalPrice = this.calculateTotalPrice();
-      let discount = 0;
-      if (totalPrice > 400) {
-        discount = totalPrice * 0.1;
-      }
-      totalPrice -= discount;
-      console.log(`Total price: $${totalPrice}`);
-      if (paymentAmount >= totalPrice) {
-        let change = paymentAmount - totalPrice;
-        console.log(`Thank you for your purchase! Your change is $${change}.`);
-      } else {
-        console.log(`You don't have enough money to purchase the items.`);
-      }
-    },
-  };
-  
-  // Test the cash register
-  cashRegister.addItem("Iphone");
-  cashRegister.addItem("Smart TV");
-  cashRegister.addItem("Phone")
-  cashRegister.addItem("Gaming Console");
-  console.log(`Total price: $${cashRegister.calculateTotalPrice()}`);
-  cashRegister.pay(800);
-  
+
+let changeStructure = document.getElementById("change")
+
+function newDiv(){
+    const div = document.getElementById("user")
+    div.innerHTML = `
+    <div>
+        <h3>What change have we made?</h3>
+        <p>You can see this because the structure of the previous div has been altered</p>
+        <p>If you click the button below you will be able to go back to the previous out look</p>
+        <button id="changeback">Change back</button>
+    </div>
+    `
+}
+
+changeStructure.addEventListener("click", newDiv)
+
+let changeText = document.getElementById("changeback")
+
+function newText(){
+    const paragraph = document.getElementById("text")
+    paragraph.innerText = "If you can see this text then the button below has been clicked"
+}
+
+changeText.addEventListener("click", newText)
