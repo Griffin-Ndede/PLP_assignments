cf722eee680360ad11c35910cfd35010c4af82aa
Griffin <omondigriffin4@gmail.com>
2024-05-15 14:37:00 +0300

    creates a 404 error handling page

--- Django/practice/ShareSpace/ShareSpace/settings.py
+++ Django/practice/ShareSpace/ShareSpace/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t^!ir_*v_=aq%*qo%qg!k^n8024@i_v^#%qspfu=q&zcx%fk#y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
